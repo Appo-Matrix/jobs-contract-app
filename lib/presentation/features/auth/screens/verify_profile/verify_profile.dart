a4f4da093f0d7e1389e95d8f5b9e491cd432d9d8
basitmurad <basitmurad7@gmail.com>
2025-01-29 22:16:42 +0500

    working in verify profile screen

---
+++ lib/presentation/features/auth/screens/verify_profile/verify_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VerifyProfile extends StatefulWidget {
+  const VerifyProfile({super.key});
+
+  @override
+  State<VerifyProfile> createState() => _VerifyProfileState();
+}
+
+class _VerifyProfileState extends State<VerifyProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
