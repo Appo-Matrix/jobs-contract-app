7963339df5c3bed3a21447f5264cf6e3cdc35276
basitmurad <basitmurad7@gmail.com>
2025-01-30 22:07:36 +0500

    working on add education screen

---
+++ lib/presentation/features/users/add_education/add_education.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AddEducation extends StatefulWidget {
+  const AddEducation({super.key});
+
+  @override
+  State<AddEducation> createState() => _AddEducationState();
+}
+
+class _AddEducationState extends State<AddEducation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
