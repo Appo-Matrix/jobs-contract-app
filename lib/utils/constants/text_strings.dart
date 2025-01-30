0baa225170d2dc9ee0104b5a13e57660985bb50f
Zulfiqar-Ali-Hunxai <zulfiqar.alihunzai05@gmail.com>
2025-01-30 14:31:19 +0500

    Signup Screen Completed

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -11,8 +11,16 @@ class PText {
   static const String forgetPassword = "Forgot password";
   static const String signIn = "Sign in";
   static const String signUp = "Sign up";
+  static const String getStart = "Get started";
+  static const String alreadyAccount = "Already have an account?";
+  static const String signupWithGoogle = "Sign up with Google";
   static const String signInGoogle = "Sign in with Google";
   static const String account = "Don’t have an account?";
-
+  static const String signupSubtitle = "Start your 30-day free trial.";
+  static const String signUpName = "Enter your name";
+  static const String signUpTitle = "Name";
+  static const String enterYourName = "Enter your email";
+  static const String createPassword = "Create a password";
+  static const String passwordMustBe = "Must be at least 8 characters.";
 
 }
