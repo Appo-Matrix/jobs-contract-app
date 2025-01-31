558a86cfe1f3933f611d083f799c5d6eca521a43
basitmurad <basitmurad7@gmail.com>
2025-01-31 18:54:57 +0500

    Start working on payments screen

---
+++ lib/presentation/features/users/payments/payments_method.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class PaymentsMethod extends StatefulWidget {
+  const PaymentsMethod({super.key});
+
+  @override
+  State<PaymentsMethod> createState() => _PaymentsMethodState();
+}
+
+class _PaymentsMethodState extends State<PaymentsMethod> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
